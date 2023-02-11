c681494048f46697ca0488c7feecfc0fb1d63f97
Wall <ilnazamirhanov61@gmail.com>
2023-02-11 22:14:49 +0300

    add header layout

---
+++ vite-project/src/components/layout/Header.vue
@@ -0,0 +1,25 @@
+<template>
+<header class="header">
+  <router-link to="/">Base Vue3</router-link>
+</header>
+</template>
+
+<script>
+export default {
+  name: "Header"
+}
+</script>
+
+<style lang="scss" scoped>
+.header {
+  background: #fff;
+  font-weight: bold;
+  padding: 0 20px;
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.07);
+  height: 62px;
+  display: flex;
+  align-items: center;
+  z-index: 1;
+  position: relative;
+}
+</style>
