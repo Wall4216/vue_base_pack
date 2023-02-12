c01b8fe85af0ff883dcd3bf4f4da564ce00b4a22
Wall <ilnazamirhanov61@gmail.com>
2023-02-12 23:40:25 +0300

    add checkboxGroup

--- vite-project/src/components/Checkbox/CheckboxGroup.vue
+++ vite-project/src/components/Checkbox/CheckboxGroup.vue
@@ -1,13 +1,48 @@
-<template>
+<script setup>
+import Checkbox from '@/components/Checkbox/Checkbox.vue'
 
-</template>
+const emit = defineEmits(['update:value'])
+const props = defineProps({
+  value: {
+    type: Array,
+    required: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  options: {
+    type: Array,
+    required: true,
+    validator: (value) => {
+      const hasNameKey = value.every((option) => Object.keys(option).includes('name'))
+      const hasIdKey = value.every((option) => Object.keys(option).includes('id'))
+      return hasNameKey && hasIdKey
+    }
+  }
+})
 
-<script>
-export default {
-  name: "CheckboxGroup"
+const check = (params) => {
+  let updateValue = [...props.value]
+  if (params.checked) {
+    updateValue.push(params.optionId)
+  } else {
+    updateValue.splice(updateValue.indexOf(params.optionId), 1)
+  }
+  emit('update:value', updateValue)
 }
 </script>
 
-<style scoped>
-
-</style>
+<template>
+  <div v-for="option in options" :key="option.id">
+    <checkbox
+        :label="option.name"
+        :id="option.id"
+        :name="name"
+        :value="option.name"
+        :checked="value.includes(option.id)"
+        group
+        @updateCheckboxGroup="check"
+    />
+  </div>
+</template>
