e82cc4ff311339d619b1f6650a41430bb74b74fb
Wall <ilnazamirhanov61@gmail.com>
2023-02-13 21:58:15 +0300

    add percent and other logics

--- vite-project/src/components/Progress/ProgressBar.vue
+++ vite-project/src/components/Progress/ProgressBar.vue
@@ -1,11 +1,27 @@
-<template>
-<div class="progress-container">
-  <div class="progress">
-    <div class="progress-bar">
+<script setup>
+const props = defineProps({
+  maxWidth: {
+    type: String,
+    default: '300px'
+  },
+  percent: {
+    type: Number,
+    required: true
+  },
+  color: {
+    type: String,
+    default: 'primary'
+  }
+})
+</script>
 
+<template>
+  <div class="progress-container" :style="[{'max-width': maxWidth}]">
+    <span class="progress-percent" :style="[{'color': `var(--${color})`}]">{{percent}}%</span>
+    <div class="progress" :style="[{'background': `var(--${color}-hover)`}]">
+      <div class="progress-bar" :style="[{'width': `${percent}%`}, {'background': `var(--${color})`}]"></div>
     </div>
   </div>
-</div>
 </template>
 
 <script>
