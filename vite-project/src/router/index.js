ce1e691376e395adb6e3474446874da2622e569c
Wall <ilnazamirhanov61@gmail.com>
2023-02-13 21:08:06 +0300

    add RadioButton

--- vite-project/src/router/index.js
+++ vite-project/src/router/index.js
@@ -2,6 +2,7 @@ import {createRouter, createWebHistory} from 'vue-router'
 import Index from '@/views/Index.vue'
 import Button from "@/views/Button.vue";
 import Checkbox from "@/views/Checkbox.vue";
+import RadioButton from "@/views/RadioButton.vue";
 import Typography from "@/views/Typography.vue";
 const router = createRouter({
     history: createWebHistory(),
@@ -25,6 +26,11 @@ const router = createRouter({
             path: '/checkbox',
             name: 'Checkbox',
             component: Checkbox
+        },
+        {
+            path: '/radiobutton',
+            name: 'Radiobutton',
+            component: RadioButton
         },
     ]
 })
---
+++ vite-project/src/views/RadioButton.vue
@@ -0,0 +1,13 @@
+<template>
+<h1>RadioButton</h1>
+</template>
+
+<script>
+export default {
+  name: "RadioButton"
+}
+</script>
+
+<style scoped>
+
+</style>
